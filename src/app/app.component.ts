f9757fa061d319a443e389d629ddc65ff1495c11
GarimaUttam <garimauttam191203@gmail.com>
2025-01-17 13:05:21 +0530

    component and single file components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from "./components/admin/admin.component";
+import { UserComponent } from './components/user/user.component';
+import { SingleFileComponentComponent } from './components/single-file-component/single-file-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AdminComponent, UserComponent, SingleFileComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
