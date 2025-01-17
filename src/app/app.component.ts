543b40b1e14585e356e3118b0600eb0b879c7035
GarimaUttam <garimauttam191203@gmail.com>
2025-01-17 13:53:34 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from "./components/admin/admin.component";
 import { UserComponent } from './components/user/user.component';
 import { SingleFileComponentComponent } from './components/single-file-component/single-file-component.component';
+import { DataBindingComponent } from "./components/data-binding/data-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AdminComponent, UserComponent, SingleFileComponentComponent],
+  imports: [AdminComponent, UserComponent, SingleFileComponentComponent, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
