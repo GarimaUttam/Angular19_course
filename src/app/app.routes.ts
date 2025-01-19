3bc44797ded5cb8ca9733fb32d5d8bba6b7932cf
GarimaUttam <garimauttam191203@gmail.com>
2025-01-19 19:58:34 +0530

    template-forms and validations using #

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { NgClassComponent } from './components/ng-class/ng-class.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { ControlStatementComponent } from './components/control-statement/control-statement.component';
+import { SignalComponent } from './components/signal/signal.component';
+import { LinkedSignalComponent } from './components/linked-signal/linked-signal.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 export const routes: Routes = [
     {
@@ -25,5 +28,17 @@ export const routes: Routes = [
     {
         path: 'control-flow',
         component:ControlStatementComponent
+    },
+    {
+        path: 'signal',
+        component:SignalComponent
+    },
+    {
+        path:'linked-signal',
+        component:LinkedSignalComponent
+    },
+    {
+        path:'template-forms',
+        component:TemplateFormComponent
     }
 ];
