82f58c1bf6a6d08f83270b0b1470e6f0b87393ff
GarimaUttam <garimauttam191203@gmail.com>
2025-01-20 17:55:20 +0530

    get API integration

--- src/app/components/API/get-api/get-api.component.ts
+++ src/app/components/API/get-api/get-api.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class GetAPIComponent {
 
-}
+  userList: any [] = [];
+  productList: any [] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+  getUsers(){
+    // the GET'` datatype is returning observable we can subscribe to that 
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((result:any) =>{
+        this.userList = result;
+    })
+  }
+
+  // third party library
+//   getProducts(){
+//     this.http.get("https://fake-store-api.mock.beecepter.com/api/products").subscribe((res:any) => {
+//       this.productList = res;
+//     })
+//   }
+// }
