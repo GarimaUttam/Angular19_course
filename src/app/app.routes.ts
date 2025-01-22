90b5ef885e2a5f526b3e63cdc986ea5259e8c589
GarimaUttam <garimauttam191203@gmail.com>
2025-01-22 13:31:30 +0530

    login and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,67 +13,82 @@ import { CustomerComponent } from './components/API/customer/customer.component'
 import { ResourceAPIComponent } from './components/resource-api/resource-api.component';
 import { LifeCycleComponent } from './components/life-cycle/life-cycle.component';
 import { NgForDirectiveComponent } from './components/ng-for-directive/ng-for-directive.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
     {
         path:'',
-        redirectTo: 'dataBinding',
+        redirectTo: 'login',
         pathMatch:'full'
     },
     {
-        path: 'admin',
-        component:AdminComponent
+        path:'login',
+        component:LoginComponent
     },
     {
-        path: 'ng-class',
-        component:NgClassComponent
-    },
-    {
-        path: 'ng-for',
-        component:NgForDirectiveComponent
-    },
-    {
-        path: 'dataBinding',
-        component:DataBindingComponent
-    },
-    {
-        path: 'control-flow',
-        component:ControlStatementComponent
-    },
-    {
-        path: 'signal',
-        component:SignalComponent
-    },
-    {
-        path:'linked-signal',
-        component:LinkedSignalComponent
-    },
-    {
-        path:'template-forms',
-        component:TemplateFormComponent
-    },
-    {
-        path:'reactive-form',
-        component:ReactiveFormComponent
-    },
-    {
-        path:'get-api',
-        component:GetAPIComponent
-    },
-    {
-        path:'post-api',
-        component:PostAPIComponent
-    },
-    {
-        path:'customer',
-        component:CustomerComponent
-    },
-    {
-        path:'resource-api',
-        component:ResourceAPIComponent
-    },
-    {
-        path:'life-cycle',
-        component:LifeCycleComponent
+        path:'',
+        component:LayoutComponent,
+        canActivate:[authGuard],
+        children:[
+            {
+                path: 'admin',
+                component:AdminComponent
+            },
+            {
+                path: 'ng-class',
+                component:NgClassComponent
+            },
+            {
+                path: 'ng-for',
+                component:NgForDirectiveComponent
+            },
+            {
+                path: 'dataBinding',
+                component:DataBindingComponent
+            },
+            {
+                path: 'control-flow',
+                component:ControlStatementComponent
+            },
+            {
+                path: 'signal',
+                component:SignalComponent
+            },
+            {
+                path:'linked-signal',
+                component:LinkedSignalComponent
+            },
+            {
+                path:'template-forms',
+                component:TemplateFormComponent
+            },
+            {
+                path:'reactive-form',
+                component:ReactiveFormComponent
+            },
+            {
+                path:'get-api',
+                component:GetAPIComponent
+            },
+            {
+                path:'post-api',
+                component:PostAPIComponent
+            },
+            {
+                path:'customer',
+                component:CustomerComponent
+            },
+            {
+                path:'resource-api',
+                component:ResourceAPIComponent
+            },
+            {
+                path:'life-cycle',
+                component:LifeCycleComponent
+            }
+        ]
     }
+   
 ];
